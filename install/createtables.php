48f9aa8add21003641750efd81183b13e769d571
Joseph Hassell <josephh2018@gmail.com>
2016-05-24 17:29:10 -0500

    Add files via upload

---
+++ install/createtables.php
@@ -0,0 +1,105 @@
+<!--
+
+The MIT License (MIT)
+
+Copyright (c) Mon May 23 2016 Joseph Hassell joseph@thehassellfamily.net
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORTOR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-->
+
+<?
+include "../sqlconnect.php";
+
+$tablesucsess = 0;
+$tablenext = "<a href=''>Please Wait</a>";
+
+// sql to create table PASSES
+$sqlpasses = "CREATE TABLE passes (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+student_id VARCHAR(10) NOT NULL,
+period VARCHAR(10) NOT NULL,
+sh_teacher VARCHAR(40) NOT NULL,
+place VARCHAR(10) NOT NULL,
+day_to_come VARCHAR(10) NOT NULL,
+request_date TIMESTAMP
+)";
+
+
+
+// sql to create table Teachers
+$sqlteachers = "CREATE TABLE teachers (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name_title VARCHAR(5) NOT NULL,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+room VARCHAR(10) NOT NULL,
+period VARCHAR(10) NOT NULL
+)";
+
+
+// sql to create table admin
+$sqladmin = "CREATE TABLE admin (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(100) NOT NULL,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+password VARCHAR(100) NOT NULL
+)";
+
+if ($conn->query($sqlpasses) === TRUE) {
+    $tablesucsess += 1;
+    echo "Table Passes created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+if ($conn->query($sqlteachers) === TRUE) {
+    $tablesucsess += 1;
+    echo "Table teachers created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+if ($conn->query($sqladmin) === TRUE) {
+    $tablesucsess += 1;
+    echo "Table teachers created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+if ($tablesucsess == 3) {
+    echo "All tables created";
+    $tablenext = "<a href='step3.php'>Next --></a>";
+} else {
+    echo "There was an error creating all the tables.";
+    $tablenext = "<a href='index.php'>Try Again</a>";
+        
+}
+
+
+
+
+
+
+$conn->close();
+?>
---
+++ install/step3.php
@@ -0,0 +1,117 @@
+<!--
+
+The MIT License (MIT)
+
+Copyright (c) Tue May 24 2016 Joseph Hassell joseph@thehassellfamily.net
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORTOR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+-->
+
+
+<html>
+
+<head>
+    <title>Passr-Install-Step 3</title>
+</head>
+
+<body>
+    <div>
+        <h1>Welcome to the Passr auto installer.</h1>
+    </div>
+    <h3>Step 3</h3>
+    <p>Now, lets create admin accounts</p>
+    <p>(You can add more later)</p>
+    <div>
+        <form method="post" action="">
+            <p>
+                <input type="text" required id="firstname" name="firstname">
+                <label for="firstname">First Name</label>
+            </p>
+            <p>
+                <input type="text" required id="lastname" name="lastname">
+                <label for="lastname">Last Name</label>
+            </p>
+            <p>
+                <input type="text" required id="username" name="usernameadmin">
+                <label for="username">Username</label>
+            </p>
+            <p>
+                <input type="password" required id="password" name="passwordadmin">
+                <label for="password">Password</label>
+            </p>
+            <p>
+                <input type="password" required id="password2" name="password2admin">
+                <label for="password2">Password(again)</label>
+            </p>
+            <p>
+                <input type="email" required id="email" name="email">
+                <label for="email">Email</label>
+            </p>
+            <p>
+                <input type="submit" name="submit" value="Create Account">
+            </p>
+        </form>
+        <p>
+            <a href="end.php">Next --></a>
+        </p>
+    </div>
+
+
+</body>
+
+</html>
+
+<?
+if(isset($_POST['submit'])){ 
+
+    
+    
+        foreach ($_POST as $key => $value) {
+
+  }
+    {
+        $first_name = $_POST['firstname'];
+        $last_name = $_POST['lastname'];
+        $usernameadmin = $_POST['usernameadmin'];
+        $email = $_POST['email'];
+        $passwordadmin = $_POST['passwordadmin'];
+        $password2admin = $_POST['password2admin'];
+
+        if ($passwordadmin != $password2admin) {
+            echo "ERROR: Passwords must match";
+        } else {
+            include "../sqlconnect.php";
+            
+            $sql = "INSERT INTO admin (username, firstname, lastname, email, password)
+            VALUES ('$usernameadmin', '$first_name', '$last_name', '$email', '$passwordadmin')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "New account created successfully";
+                
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+                
+            }
+
+            $conn->close();
+        }
+    }
+    
+    
+} 
+?>
