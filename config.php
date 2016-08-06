ed570fbdef44f647d728247fa862e350ef70e112
Joseph Hassell <josephh2018@gmail.com>
2016-08-06 16:28:50 -0500

    removed old limit vars

--- config.php
+++ config.php
@@ -4,11 +4,5 @@ $servername = "localhost";
 $dbusername = "uname"; 
 $dbpassword = "pass"; 
 $database = "dbname";
-//Limit # Of Students 
-$lecslimit = 3;
-$mathslimit = 4;
-$libraryslimit = 4;
-$hdslimit = 16;
-$WritingLabslimit = 3;
-$ForeignLanguageslimit = 2;
+
 ?>
