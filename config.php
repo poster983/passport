cb9261205723dc2be6b6750c782e433d78a77dad
Joseph Hassell <josephh2018@gmail.com>
2016-08-16 20:02:43 -0500

    Added Debug Toggle

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?
 //Server Login
-$servername = "localhost"; 
-$dbusername = "uname"; 
-$dbpassword = "pass"; 
+$servername = "localhost";
+$dbusername = "uname";
+$dbpassword = "pass";
 $database = "dbname";
 
+//Set to "0" to disable debug echos
+$devDebugEchoToggle = 1;
 ?>
